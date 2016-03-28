2c0510bda912bbdad2d524596401f1963ed4826b
TommyYE <TommyYE@Tommy-mac.local>
2016-03-28 20:07:47 +1000

    for MAMP

--- connectMySQL.php
+++ connectMySQL.php
@@ -1,14 +1,22 @@
 <?php
-$server='localhost';
-$user='root';
-$pass='123456'
-$db='test';
+$user = 'root';
+$password = 'root';
+$db = 'Test';
+$host = 'localhost';
+$port = 8889;
 
-//Conect to database
-$conn=mysqli_connect($server,$user,$pass,$db);
-if (mysqli_connect_errno($conn)) {
-	echo "Fail to connect: ".mysqli_connect_error();
-} else {
-	echo "Connection ok.\n"
-}
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link, 
+   $host, 
+   $user, 
+   $password, 
+   $db,
+   $port
+);
+if (mysqli_connect_errno($success))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  } else { echo "Connection was OK!\n";}
+
 ?>
